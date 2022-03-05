064bb8e4c685088ec10f43bff68e93eef8827a7d
aForero-GitHub <foreromartinez.andres@gmail.com>
2022-03-04 19:24:07 -0500

    Create Date class

---
+++ POOConceptsConsole/POOConceptsConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POOConceptsConsole;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("=============");
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(2021, 11, 04));
+    Console.WriteLine(new Date(2000, 5, 10));
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
