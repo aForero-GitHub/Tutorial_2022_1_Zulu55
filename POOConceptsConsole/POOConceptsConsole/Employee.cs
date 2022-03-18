2e07b167e8a442c466b2f5dbe9255cab3cb4abdf
aForero-GitHub <foreromartinez.andres@gmail.com>
2022-03-17 22:55:30 -0500

    Employee, SalaryEmployee, CommissionEmployee

---
+++ POOConceptsConsole/POOConceptsConsole/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace POOConceptsConsole
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Date BirthDay { get; set; }
+
+        public Date HiringDate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+            return $"{Id} - {FirstName} {LastName}, " +
+                $"Birth: {BirthDay}, " +
+                $"Hiring: {HiringDate}, " +
+                $"Is Active: {IsActive}";
+        }
+
+    }
+}
--- POOConceptsConsole/POOConceptsConsole/Program.cs
+++ POOConceptsConsole/POOConceptsConsole/Program.cs
@@ -1,18 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using POOConceptsConsole;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("POO Concepts");
 
 Console.WriteLine("=============");
 
 try
 {
-    Console.WriteLine(new Date(2024, 2, 29));
-    Console.WriteLine(new Date(2021, 11, 04));
-    Console.WriteLine(new Date(2000, 5, 10));
+    Employee employee1 = new SalaryEmployee()
+    {
+        Id = 1010,
+        FirstName = "Sandra",
+        LastName = "Morales",
+        BirthDay = new Date(1990, 5, 23),
+        HiringDate = new Date(2022, 1, 15),
+        IsActive = true,
+        Salary = 1815453.45M
+    };
+
+    Employee employee2 = new CommissionEmployee()
+    {
+        Id = 1013,
+        FirstName = "Juan",
+        LastName = "Cortez",
+        BirthDay = new Date(2000, 10, 23),
+        HiringDate = new Date(2022, 3, 15),
+        IsActive = true,
+        CommissionPercentaje = 0.03F,
+        Sales = 320000000M
+    };
+
+    Console.WriteLine(employee1);
+
+    Console.WriteLine(employee2);
 }
 catch (Exception ex)
 {
-
     Console.WriteLine(ex.Message);
 }
